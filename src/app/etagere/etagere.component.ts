6dd3eeb1cac32eff436fa3c6e67f60424a1c90e6
issude-seukour <sosthene.berenger@sfr.fr>
2022-04-26 20:35:03 +0200

    1 file modified, 2 added

---
+++ src/app/etagere/etagere.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etagere',
+  templateUrl: './etagere.component.html',
+  styleUrls: ['./etagere.component.css']
+})
+export class EtagereComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
